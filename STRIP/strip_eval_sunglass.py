bf625050c4a73f226f670f453b37dfe9bfc86654
kyh275 <kyh275@nyu.edu>
2020-12-22 13:36:43 -0500

    update strip and its readme

--- STRIP/strip_eval_sunglass.py
+++ STRIP/strip_eval_sunglass.py
@@ -10,17 +10,34 @@ model_filename = 'models/sunglasses_bd_net.h5'
 clean_data_filename = 'data/clean_validation_data.h5'
 
 
+def data_loader(filepath):
+    data = h5py.File(filepath, 'r')
+    x_data = np.array(data['data'])
+    y_data = np.array(data['label'])
+    x_data = x_data.transpose((0, 2, 3, 1))
+
+    return x_data, y_data
+
+
+def data_preprocess(x_data):
+    return x_data/255
+
+
 def main():
     bd_model = keras.models.load_model(model_filename)
     clean_dataset = h5py.File(clean_data_filename, 'r')
     strip = STRIP.Strip(bd_model, clean_dataset, False)
 
-    img = io.imread(img_path)
-    image_without_alpha = img[:, :, :3]
-    image_resized = transform .resize(
-        image_without_alpha, (55, 47), anti_aliasing=True)
-    img_data = np.array([image_resized]);
-    y = strip.predic('b1', img_data)
+    if img_path.endswith('.h5'):
+        x, _ = data_loader(img_path)
+    else:
+        x = io.imread(img_path)
+        x = x[:, :, :3]
+        x = transform .resize(
+            x, (55, 47), anti_aliasing=True)
+        x = np.array([x])
+
+    y = strip.predic('b1', x)
     print('Predict label: ', y[0])
 
 
